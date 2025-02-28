9e1104144c63d4e4dd9325bb973b883e9be78d62
taichan_ <taichan.pia@gmail.com>
2025-02-28 19:21:08 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,31 @@
 const areas = [
-    {name: "エリア1", img: "img/areaA.jpg", link: "area1.html"},
-    {name: "エリア2", img: "img/areaB.jpg", link: "area2.html"},
-    {name: "エリア3", img: "img/areaC.jpg", link: "area3.html"}
+    {name: "エリア1", img: "images/area1.jpg", link: "area1.html"},
+    {name: "エリア2", img: "images/area2.jpg", link: "area2.html"},
+    {name: "エリア3", img: "images/area3.jpg", link: "area3.html"}
 ];
 
 let currentArea = 0;
 
-function changeArea(direction) {
+// 音をすぐ鳴らす処理
+function playButtonSound() {
     const sound = document.getElementById('button-sound');
+    sound.currentTime = 0;  // 連続押し対応
     sound.play();
+}
+
+// エリア切り替え処理
+function changeArea(direction) {
+    playButtonSound();
 
     const img = document.getElementById('area-image');
     const areaName = document.getElementById('area-name');
 
-    // フェードアウト
+    // フェードアウト開始
     img.style.opacity = '0';
 
     setTimeout(() => {
         currentArea = (currentArea + direction + areas.length) % areas.length;
+
         img.src = areas[currentArea].img;
         areaName.textContent = areas[currentArea].name;
 
@@ -26,12 +34,8 @@ function changeArea(direction) {
     }, 800); // フェードアウト後に画像切替
 }
 
+// 移動ボタン押下時処理
 function moveToPage() {
-    const sound = document.getElementById('button-sound');
-    sound.play();
-
-    const targetPage = areas[currentArea].link;
-    setTimeout(() => {
-        window.location.href = targetPage;
-    }, 200); // 音が鳴った後で遷移
+    playButtonSound();
+    window.location.href = areas[currentArea].link;
 }
