c965279d2a5a47776def80d47ddd8c8230f7b4d4
taichan_ <taichan.pia@gmail.com>
2025-02-28 17:48:36 +0900

    Update script.js

--- script.js
+++ script.js
@@ -5,13 +5,11 @@ const links = {
     right: 'menu.html'
 };
 
-// ボタン押した時の遷移
 document.querySelector('.top-button').addEventListener('click', () => goToPage(links.top));
 document.querySelector('.bottom-button').addEventListener('click', () => goToPage(links.bottom));
 document.querySelector('.left-button').addEventListener('click', () => goToPage(links.left));
 document.querySelector('.right-button').addEventListener('click', () => goToPage(links.right));
 
-// ページ遷移共通処理
 function goToPage(url) {
     document.body.classList.add('fade-out');
     setTimeout(() => {
