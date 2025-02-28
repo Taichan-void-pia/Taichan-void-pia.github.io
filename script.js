2b7bae8d68002fc2d9a58e4bedaa5cc866e19fe4
taichan_ <taichan.pia@gmail.com>
2025-03-01 00:35:30 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const areas = [
-    {name: "エリア1", img: "img/areaA.png", link: "https://nemonowa.github.io/"},
-    {name: "エリア2", img: "img/areaB.png", link: "area2.html"},
-    {name: "エリア3", img: "img/areaC.png", link: "area3.html"}
+    {name: "Project-Nexus", img: "img/areaA.png", link: "https://nemonowa.github.io/"},
+    {name: "LanahiroMOD", img: "img/areaB.png", link: "area2.html"},
+    {name: "Home", img: "img/areaC.png", link: "area3.html"}
 ];
 
 let currentArea = 0;
